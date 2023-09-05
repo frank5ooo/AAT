ec9d5789962f1e6d2fdee6f245f57016c33e791f
frank5ooo <francolembo04@gmail.com>
2023-09-05 00:01:13 -0300

    Ya anda github actions

--- src/tarjeta.php
+++ src/tarjeta.php
@@ -8,7 +8,9 @@ class Tarjeta {
     }
 
     public function recargar($monto) {
-        $cargasAceptadas = [150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 2500, 3000, 3500, 4000];
+        $cargasAceptadas = [150, 200, 250, 300, 350, 400, 450, 500, 
+        600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 
+        2000, 2500, 3000, 3500, 4000];
         if (in_array($monto, $cargasAceptadas)) {
             $this->saldo += $monto;
             if ($this->saldo > $this->limite_saldo) {
