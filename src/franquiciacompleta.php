5bd38d90f60a439a49aeece7b8de0764d23497f1
frank5ooo <francolembo04@gmail.com>
2023-10-16 21:58:07 -0300

    Correccion del Merge

--- src/franquiciacompleta.php
+++ src/franquiciacompleta.php
@@ -1,39 +1,41 @@
 <?php
-
 namespace TrabajoSube;
-
 use TrabajoSube\tarjeta;
-
 class FranquiciaCompleta extends Tarjeta 
 {
     protected $viajesHoy = 0;
     protected $cantViajesGratis=2;
-
     public function descontarFranquiciaCompleta($boleto) 
     {   
         $tiempoActual = $this->tiempo->time();
 
+        $diaSemana= date('l', $this->tiempo->time());
+        $horaDia= date('G', $this->tiempo->time());
+
         if(intval($this->viajesHoy/86400) < intval($this->tiempo->time()/86400))
         {
             $this->cantViajesGratis=2;
-        }       
-
-        if($this->cantViajesGratis > 0)
-        {
-            $this->cantViajesGratis--;
-            $this->viajesHoy = $tiempoActual;
-            return true;
         }
-        
+        if($diaSemana !== "Sunday" && $diaSemana !== "Saturday" && $horaDia >=6 && $horaDia <=22)
+        {
+            if($this->cantViajesGratis > 0)
+            {
+                $this->cantViajesGratis--;
+                $this->viajesHoy = $tiempoActual;
+                return true;
+            }
+            else
+            {
+                return $this->descontar($boleto);
+            }
+        }
         else
         {
             return $this->descontar($boleto);
         }
     }
-
     public function getCantViajesGratis()
     {
         return $this->cantViajesGratis;
     }
-
 }
--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -5,7 +5,6 @@ namespace TrabajoSube\Tests;
 use PHPUnit\Framework\TestCase;
 use TrabajoSube\tarjeta;
 use TrabajoSube\tiempoFalso;
-use TrabajoSube\lineaInterUrbano;
 
 
 class TarjetaTest extends TestCase {
@@ -13,9 +12,7 @@ class TarjetaTest extends TestCase {
                 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 
                 2500, 3000, 3500, 4000];
 
-    private $boletoUrbano = 120;
-    private $boletoInterUrbano = 184;
-
+    private $boleto = 120;
     public function testRecargar() 
     {
         $tiempo = new TiempoFalso;
@@ -32,42 +29,27 @@ class TarjetaTest extends TestCase {
     
             $this->assertEquals($saldoEsperado, $tarjeta->getSaldo());
         }
-        
     }
 
-    public function testDescontarUrbano()
+    public function testDescontar()
     {
         $tiempo = new TiempoFalso;
+            
+        foreach ($this->montosValidos as $prueba) 
+        {
 
-        $tarjeta = new Tarjeta(1,$tiempo);
+            $tarjeta = new Tarjeta(1,$tiempo);
 
-        $tarjeta->recargar(2000);
-        
-        $resultado = $tarjeta->descontar($this->boletoUrbano);
+            $tarjeta->recargar($prueba);
+            
+            $resultado = $tarjeta->descontar($this->boleto);
 
-        $this->assertTrue($resultado);
-        $this->assertEquals(2000 - $this->boletoUrbano, $tarjeta->getSaldo());
+            $this->assertTrue($resultado);
+            $this->assertEquals($prueba - $this->boleto, $tarjeta->getSaldo());
+        }
     }
 
-    public function testDescontarInterUrbano()
-    {   
-        $interUrbano = new lineaInterUrbano(144);
-        $boletoInterUrbano = $interUrbano->getPrecio();
-
-        $tiempo = new TiempoFalso;
-
-        $tarjeta = new Tarjeta(1,$tiempo);
-
-        $tarjeta->recargar(2000);
-        
-        $resultado = $tarjeta->descontar($boletoInterUrbano);
-
-        $this->assertTrue($resultado);
-        $this->assertEquals(2000 - $boletoInterUrbano, $tarjeta->getSaldo());
-    }
-
-  
-    public function testDescontarSinSaldoUrbano()
+    public function testDescontarSinSaldo()
     {
         $tiempo = new TiempoFalso;
 
@@ -75,38 +57,21 @@ class TarjetaTest extends TestCase {
 
         $tarjeta->recargar(150);
         
-        $tarjeta->descontar($this->boletoUrbano);   //viaje 1
+        $tarjeta->descontar($this->boleto);   //viaje 1
 
-        $resultado = $tarjeta->descontar($this->boletoUrbano);   //viaje 2
+        $resultado = $tarjeta->descontar($this->boleto);   //viaje 2
 
         $this->assertTrue($resultado);
         $this->assertEquals(-90, $tarjeta->getSaldo());
 
-        $resultado = $tarjeta->descontar($this->boletoUrbano);   //viaje 3
-
-        $this->assertFalse($resultado);
-
-    }
-
-    public function testDescontarSinSaldoInterUrbano()
-    {   
-        $interUrbano = new lineaInterUrbano(144);
-        $tiempo = new TiempoFalso;
-
-        $tarjeta = new Tarjeta(1,$tiempo);
-
-        $boletoInterUrbano = $interUrbano->getPrecio();
-
-        $tarjeta->recargar(150);
-        
-        $resultado = $tarjeta->descontar($boletoInterUrbano);   //viaje 2
+        $resultado = $tarjeta->descontar($this->boleto);   //viaje 3
 
         $this->assertTrue($resultado);
-        $this->assertEquals(-34, $tarjeta->getSaldo());
+        $this->assertEquals(-210, $tarjeta->getSaldo());
+        
+        $resultado = $tarjeta->descontar($this->boleto);   //viaje 4
 
-        $resultado = $tarjeta->descontar($boletoInterUrbano); 
         $this->assertFalse($resultado);
-
     }
 
     public function testRecargaConExcedente()
@@ -115,9 +80,10 @@ class TarjetaTest extends TestCase {
         $tarjeta = new Tarjeta(1,$tiempo);
         $tarjeta->recargar(4000);  
         $tarjeta->recargar(4000);  
-    
+        
+        //echo"saldo". $tarjeta->getSaldo();
         $this->assertEquals(6600, $tarjeta->getSaldo());
-        $this->assertEquals(1400, $tarjeta->getSaldoPendiente());
+       // $this->assertEquals(1400, $tarjeta->saldoPendiente);
     }
 
     public function testRecargaConExcedenteDespuesDeViaje()
@@ -127,9 +93,83 @@ class TarjetaTest extends TestCase {
         $tarjeta->recargar(4000);
         $tarjeta->recargar(4000);
         
-        $tarjeta->descontar($this->boletoInterUrbano);
+        $tarjeta->descontar($this->boleto);
         $this->assertEquals(6600, $tarjeta->getSaldo());
-        $this->assertEquals(1216 , $tarjeta->getSaldoPendiente());
+        //$this->assertEquals(1280 , $tarjeta->saldoPendiente);
     } 
+
+    public function testUnViaje()
+    {
+        $tiempo = new TiempoFalso;
+        $tarjeta = new Tarjeta(1,$tiempo);
+        $tarjeta->recargar(4000);
+        
+        $tarjeta->descontar($this->boleto);
+
+        $this->assertEquals(1,$tarjeta->getCantViajesEnElMes());
+        $this->assertEquals(date('m', $tiempo->time()),$tarjeta->getMesActual());
+    }
+
+    public function testViajeDescuento20PorCiento()
+    {
+        $tiempo = new TiempoFalso;
+        $tarjeta = new Tarjeta(1,$tiempo);
+        $tarjeta->recargar(4000);
+        
+        for($i=0; $i<31; $i++)
+        {
+            $tarjeta->descontar($this->boleto);
+        }
+        // echo"\nprecioTestt:".$tarjeta->getCantViajesEnElMes();
+        // echo"\nprecioTestt:".$tarjeta->getPrecio();
+
+        $this->assertEquals(31, $tarjeta->getCantViajesEnElMes());
+        $this->assertEquals(96, $tarjeta->getPrecio());
+    }
+
+    public function testViajeDescuento25PorCiento()
+    {
+        $tiempo = new TiempoFalso;
+        $tarjeta = new Tarjeta(1,$tiempo);
+        $tarjeta->recargar(40000);
+        
+        for($i=0; $i<90; $i++)
+        {
+            $tarjeta->descontar($this->boleto);
+        }
+        // echo"\nViajesTestt:".$tarjeta->getCantViajesEnElMes();
+        // echo"\nprecioTestt:".$tarjeta->getPrecio();
+
+        $this->assertEquals(90, $tarjeta->getCantViajesEnElMes());
+        $this->assertEquals(90, $tarjeta->getPrecio());
+    }
+
+    public function testCambioDeMes()
+    {
+        $tiempo = new TiempoFalso;
+        $tarjeta = new Tarjeta(1,$tiempo);
+        $tarjeta->recargar(40000);
+        
+        for($i=0; $i<40; $i++)
+        {
+            $tarjeta->descontar($this->boleto);
+        }
+        // echo"\nViajesTestt:".$tarjeta->getCantViajesEnElMes();
+        // echo"\nprecioTestt:".$tarjeta->getPrecio();
+        $this->assertEquals(40, $tarjeta->getCantViajesEnElMes());
+        $this->assertEquals(96, $tarjeta->getPrecio());
+        $this->assertEquals(date('m', $tiempo->time()), $tarjeta->getMesActual());
+        //echo "mes". date('m', $tiempo->time());
+
+        $tiempo->avanzar(32*86400);
+
+        $tarjeta->descontar($this->boleto);
+        //echo "\nPRECIOOOOO".  $tarjeta->getPrecio();
+
+        $this->assertEquals(1, $tarjeta->getCantViajesEnElMes());
+        $this->assertEquals(date('m', $tiempo->time()), $tarjeta->getMesActual());
+
+        $this->assertEquals(120, $tarjeta->getPrecio());
+    }
 }
 ?>
